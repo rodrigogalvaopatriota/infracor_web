8ec6eb72df5492aca8b36967a1755ab078a18554
iaparaempresas <31571350+rodrigogalvaopatriota@users.noreply.github.com>
2025-07-21 14:20:15 -0300

    Add files via upload

--- infra_st.py
+++ infra_st.py
@@ -38,7 +38,7 @@ class Dashboard:
                 ),
                 color=f"{nome_y}:N",
                 tooltip=[
-                    alt.Tooltip("nome_dia_abertura:N", title="nome_dia_abertura"),
+                    alt.Tooltip(f"{nome_y}:N", title="nome_dia_abertura"),
                     #alt.Tooltip("status_distancia:N", title="Status de Distância"),
                     alt.Tooltip("den:Q", title="Quantidade"),
                     #alt.Tooltip("percentual:Q", title="Percentual (%)", format=".2f"),  # Mostrar o percentual
