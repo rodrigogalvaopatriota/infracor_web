6854698abf349bd0efc78cf4c5e3ca5a2a87d176
iaparaempresas <31571350+rodrigogalvaopatriota@users.noreply.github.com>
2025-07-21 14:26:49 -0300

    Add files via upload

--- infra_st.py
+++ infra_st.py
@@ -57,9 +57,9 @@ class Dashboard:
             .mark_text(dy=-10, size=10, color="black")  # Ajusta a posição e aparência do texto
             .encode(
                 x=alt.X("prioridade_ba:N"),
-                y=alt.Y("den:Q"),
+                #y=alt.Y("den:Q"),
                 detail="nome_dia_abertura:N",
-                
+
                 y=alt.Y("den:Q", aggregate="sum"),
                 text=alt.Text("den:Q", aggregate="sum", format=".0f")
                 #text=alt.Text("percentual:Q", format=".1f"), #  Formatar percentual com uma casa decimal
